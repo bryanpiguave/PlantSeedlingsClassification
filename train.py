413ee51c1b35e17bd99d3d27f6f24142e2be9644
bryanpiguave <bpiguave@espol.edu.ec>
2022-06-30 22:28:55 -0500

    Adding train.py and hydra conf

--- train.py
+++ train.py
@@ -1,30 +1,102 @@
 import pathlib
 import tensorflow as tf
-import argparse
-import configparser
-from src.models import model
+import matplotlib.pyplot as plt
+import datetime 
+import hydra
+from hydra.core.config_store import ConfigStore
+from src.config import SeedlingConfig
+import wandb
+import tensorflow.compat.v1 as tf1
+
+today=datetime.date.today()
+today = today.strftime("%d-%m-%Y")
+
+cs =ConfigStore.instance()
+cs.store(name="Seedling_config",node=SeedlingConfig)
+
+@hydra.main(config_path="configs",config_name="train")
+def main(cfg:SeedlingConfig):
+  data_dir = pathlib.Path(cfg.paths.data_dir)
+  
+
+  train_dir = f"{data_dir}/train"
+  IMG_SIZE = (cfg.params.img_width, cfg.params.img_height)
+  train_ds = tf.keras.utils.image_dataset_from_directory(
+    train_dir,
+    labels="inferred",
+    validation_split=0,
+    seed=123,
+    label_mode='int',
+    color_mode="rgb",
+    image_size=IMG_SIZE,
+    batch_size=cfg.params.batch_size
+    )
+  data_augmentation = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip('horizontal'),
+    tf.keras.layers.RandomRotation(0.2),
+  ])
+  
+  IMG_SHAPE = IMG_SIZE + (3,)
 
 
-data_dir="/home/inari/PlantSeedlingsClassification"
-data_dir = pathlib.Path(data_dir)
+  preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+  rescale = tf.keras.layers.Rescaling(1./127.5, offset=-1)
 
-batch_size = 32
-img_height = 180
-img_width = 180
+  base_model = tf.keras.applications.MobileNetV2(input_shape=(IMG_SHAPE),
+                                                include_top=False,
+                                                weights='imagenet')
+  prediction_layer = tf.keras.layers.Dense(cfg.params.num_classes) 
+  global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+
+  inputs = tf.keras.Input(shape=(cfg.params.img_width, cfg.params.img_height, 3))
+  x = data_augmentation(inputs)
+  x = preprocess_input(x)
+  x = base_model(x, training=False)
+  x = global_average_layer(x)
+  x = tf.keras.layers.Dropout(0.2)(x)
+  outputs = prediction_layer(x)
+
+  model = tf.keras.Model(inputs, outputs)
 
 
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+  model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=cfg.params.base_learning_rate),
+    loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+  wandb.init(project="SeedlingClassification",
+    config=tf1.flags.FLAGS, sync_tensorboard=True)
 
 
+  history = model.fit(train_ds,
+                      epochs=cfg.params.initial_epochs,
+                      )
 
-model.compile(
-  optimizer='adam',
-  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-  metrics=['accuracy'])
+
+  acc = history.history['accuracy']
+
+  loss = history.history['loss']
+
+  plt.figure(figsize=(8, 8))
+  plt.subplot(2, 1, 1)
+  plt.plot(acc, label='Training Accuracy')
+  plt.legend(loc='lower right')
+  plt.ylabel('Accuracy')
+  plt.ylim([min(plt.ylim()),1])
+  plt.title('Training and Validation Accuracy')
+
+  plt.subplot(2, 1, 2)
+  plt.plot(loss, label='Training Loss')
+  plt.legend(loc='upper right')
+  plt.ylabel('Cross Entropy')
+  plt.ylim([0,1.0])
+  plt.title('Training and Validation Loss')
+  plt.xlabel('epoch')
+  plt.savefig("results/metrics.jpg")
+
+  saved_model_path = f"runs/model_{today}"
+  tf.saved_model.save(model, saved_model_path)
+
+if __name__=="__main__":
+  main()
